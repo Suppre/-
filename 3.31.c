0d207ff459552998c607ed2e291ed45e74940ab9
Suppre <129088003+Suppre@users.noreply.github.com>
2023-03-31 17:53:10 +0800

    Add files via upload

---
+++ 3.31.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	char arr1[] = "you are really helpful!";
+//	char arr2[] = "***********************";
+//	int a = 0;
+//	while (arr1 <= arr2)
+//	{
+//		arr2[a] = arr1[a];
+//		printf("%s\n", arr2);
+//		Sleep(1000);
+//		system("cls");
+//		a++;
+//	}
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n, i;
+//	int x = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		x = x * i;
+//	}
+//	printf("%d", x);
+//	return 0;
+//}
+
