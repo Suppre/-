76b870c8dbf5592844d1721dfe11e77401437c9e
Suppre <129088003+Suppre@users.noreply.github.com>
2023-04-05 21:35:26 +0800

    Add files via upload

---
+++ 4.5.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+
+int main()
+{	
+	float a[7] = { 0 },t;
+	float x = 0;
+	float y = 0;
+	int i;
+	for (i = 1; i <= 7; i++)
+	{
+		printf("输入第%d次分数",i);
+		scanf("%f", &a[i]);
+	}
+	for (i = 1; i <= 7; i++)
+	{
+		if (a[i] > a[i + 1])
+		{
+			t = a[i];
+			a[i] = a[i + 1];
+			a[i + 1] = t;
+		}
+	}
+	for (i = 2; i <= 6; i++)
+	{
+		x += a[i];
+	}
+	y = x / 5;
+	printf("平均成绩：%f\n", y);
+	return 0;
+}
+
+
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (a % 2 == 0)
+//	{
+//		printf("这个数是偶数");
+//	}
+//	else
+//	{
+//		printf("这个数是奇数");
+//	}
+//	return 0;
+//}
